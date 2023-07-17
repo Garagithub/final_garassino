8235a073ea02ba00d0510ae16be62b7fd0e137c1
Garagithub <119074045+Garagithub@users.noreply.github.com>
2023-07-17 11:03:50 -0300

    gg

---
+++ App.js
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native'
+ 
+export default function App() {
+  return(
+    <View className='bg-red-700 h-screen'>
+      <Text className='text-lg text-white'> Hola </Text>
+       </View>
+  );
+}
